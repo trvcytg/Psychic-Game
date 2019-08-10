c2d31673b769c33f8050925eea30c87ad0cb833c
Todd Tracy <trvcytg@gmail.com>
2019-08-10 14:16:07 -0500

    Added user choice for number of guesses with a default of 10, also added alerts for win and loss.

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -33,16 +33,40 @@ const alphabet = [
 // Guesses Left: id= `tries`
 // Letters Guessed: id= `guessed`
 
+// function for user to choose number of tries
+function tryHard() {
+  let tries = prompt("How many tries would you like?", `${10}`);
+  tries = parseInt(tries, 10);
+  if (Number.isInteger(tries) > 0) {
+    triesText.textContent = tries;
+    return tries;
+  } else if (isNaN(tries)) {
+    tries = 10;
+    triesText.textContent = tries;
+    console.log(`default ${tries}`);
+    return tries;
+  } else {
+    alert(`That's not a number!`);
+    tryHard();
+  }
+}
+
 // create variables and their reference points in the html.
 let wins = 0;
 let losses = 0;
-let tries = -10;
+let tries = 10;
+// let tries = 10;
+// let tries = prompt("How many tries would you like?", `${10}`);
+
 let guessed = [];
 
 let winsText = document.getElementById("wins");
 let lossesText = document.getElementById("losses");
 let triesText = document.getElementById("tries");
 let guessedText = document.getElementById("guessed");
+
+// Call function for user to determine number of tries
+tries = tryHard();
 
 // function to determine Computer's letter choice
 function compChoice() {
@@ -53,8 +77,7 @@ function compChoice() {
 // function to start new game upon completion of old game
 function newGame() {
   compChoice();
-  tries = -10;
-  triesText.textContent = -tries;
+  tries = tryHard();
   guessedText.textContent = "Make your first guess!";
   lossesText.textContent = losses;
   guessed.length = 0;
@@ -70,31 +93,36 @@ document.onkeyup = function playGame(event) {
     if (userGuess == computerGuess) {
       wins++;
       console.log(`winner winner, chicken dinner`);
+      alert(`Winner!! Congrats, you clairvoyant fool.`);
       newGame();
       guessedText.textContent = guessed;
     } else {
       console.log(`wrong.`);
-      tries++;
-      triesText.textContent = -tries;
+      tries--;
+      triesText.textContent = tries;
       guessed.push(` ${userGuess}`);
       console.log(`You've already guessed: ${guessed}`);
       guessedText.textContent = guessed;
     }
-    console.log(`Number of tries remaining: ${-tries}`);
+    console.log(`Number of tries remaining: ${tries}`);
     if (tries == 0) {
       losses++;
+      console.log(`well, I guess you tried, still a loser though.`);
+      alert(
+        `Well.. I wouldn't recommend trying the lottery anytime soon. You lost.`
+      );
       newGame();
       guessedText.textContent = guessed;
     }
   } else if (
     userGuess == `meta` ||
     userGuess == `shift` ||
-    userGuess == `capslock` ||
+    userGuess == `capslock`
   ) {
   } else {
     alert(`That's not even a letter.... you wasted one of your guesses..`);
-    tries++;
-    triesText.textContent = -tries;
+    tries--;
+    triesText.textContent = tries;
     guessed.push(` ${userGuess}`);
     console.log(`You've already guessed: ${guessed}`);
     guessedText.textContent = guessed;
